3869f94c33c115b9516cda340cb7306624048ea7
Akkay <nguyenhung.dev.info@gmail.com>
2024-04-10 12:05:12 +0700

    Revert "update"
    This reverts commit 6c537edd32bf95cfad517ad97b066bba1a8c4f4b.

--- app/src/main/java/com/example/quanlycuahangbandoan/constants/Resource.java
+++ app/src/main/java/com/example/quanlycuahangbandoan/constants/Resource.java
@@ -1,6 +1,6 @@
 package com.example.quanlycuahangbandoan.constants;
 
 public class Resource {
-    public static String RESOURCE_PATH = "android.resource:/drawable/sandwich";
+    public static String RESOURCE_PATH = "android.resource://hcmute.edu.vn.store/drawable/sandwich";
 
 }
